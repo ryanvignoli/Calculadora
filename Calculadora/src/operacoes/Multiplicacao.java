5042402368d5d617223640d678402a9fa45e1590
ryanvignoli <krameknicolas@gmail.com>
2026-04-08 14:37:38 -0300

    feat: implementa a funçao de multiplicaçao

---
+++ Calculadora/src/operacoes/Multiplicacao.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operacoes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author info226
+ */
+public class Multiplicacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(x * y); 
+    }
+}
